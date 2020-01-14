7375c7eeee790ee6dec6d1bcb208f3fa0fc04b9b
Jun4871 <redtie1004@gmail.com>
2020-01-14 08:53:28 +0000

    function

---
+++ function.R
@@ -0,0 +1,17 @@
+# function 함수 활용 
+
+ryu <- function(x) {
+  print(x*5)
+}
+
+ryu(4)
+
+if_func <- function(x) {
+  if(x > 10) {
+    print("10보다 커요")
+  } else {
+    print("10보다 크지 않아요")
+  }
+}
+
+if_func(100)
