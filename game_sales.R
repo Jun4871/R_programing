470c08e322ac57b37df4e4e41dbdfdfb52fbfc29
Jun4871 <redtie1004@gmail.com>
2020-04-03 09:03:32 +0000

    R

--- game_sales.R
+++ game_sales.R
@@ -1,7 +1,12 @@
-dau <- read.csv("section3_dpu.csv", header = T, stringsAsFactors = F)
+dpu <- read.csv("section3_dpu.csv", header = T, stringsAsFactors = F)
 
-head(dau)
+head(dpu)
 
 summary(dau)
 
 str(dau)
+
+dau <- read.csv("section_dau.csv",header = T, stringsAsFactors = F) # 쉼표구분으로 다시 바꾸기 
+
+head(dau)
+
