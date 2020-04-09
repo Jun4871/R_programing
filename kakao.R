c65a500a11a0b03f5d8a664fe2ebe4b859163855
Jun4871 <redtie1004@gmail.com>
2020-04-09 13:03:24 +0000

    R

--- kakao.R
+++ kakao.R
@@ -14,54 +14,72 @@ head(raw_df, 30)
 comma_index <- raw_df %>% 
   str_detect(",")
 
+head(comma_index, 10)
+
 # comma가 있는 것만 재정렬
 raw_df <- raw_df[comma_index]
 
 length(raw_df)
-
+head(raw_df, 10)
 ## 날짜 데이터 
 
 # 첫번째 comma 위치 찾는 함수
 index_data <- regexpr(",", raw_df)
-
+#raw_df
 # comma 별 글자선택
 kakao_date <- str_sub(raw_df,1,index_data - 1)
-
+head(kakao_date, 20)
 
 ## user_name 테스트 선정
-
 nchar_max <- max(nchar(raw_df))
+# which.max(nchar(raw_df))
 
 # raw_df[which(nchar(raw_df) == 269)]
 kakao_text <- str_sub(raw_df, index_data + 2, nchar_max)
-
+head(kakao_text)
 
 # : 있는것
-text_colon_index <- grepl(":",kakao_text)
+text_colon_index <- (grepl(":",kakao_text))
+(grep(":", kakao_text, value = TRUE)
 
+length(kakao_text)
 kakao_colon_text <- kakao_text[text_colon_index]
-
+length(kakao_colon_text)
 
 # 첫번재 콜론 위치찾는 함수
 first_colon_index <- regexpr(":", kakao_colon_text)
 
 # user_name 찾기
 kakao_user_name <- str_sub(kakao_colon_text, 1, first_colon_index-2)
-
+head(kakao_user_name, 30)
 
 # message 찾기
 kakao_message <- str_sub(kakao_colon_text, first_colon_index+2, nchar_max)
 
-
+head(kakao_message, 30)
 
 
 # : 없는것 
-text_not_colon_index <- !text_colon_index
-
+text_not_colon_index <- !text_colon_index # : 기호가 없는 것은 TRUE로 뜨고, : 기호가 있는 것은 FALSE 뜬다. 
+# 데이터가 누락될 수 있기 때문에 반대되는 데이터도 모두 처리해준다. 
+length(text_not_colon_index)
+# 위에서 만든 인덱스를 다시 kakao_text라는 대화가 있는 메세지에 색인을 새겨 할당한다. 메세지에서 누락된 내용을 다시 찾는 과정이다. 
 kakao_not_colon_text <- kakao_text[text_not_colon_index]
 
 
+head(kakao_user_name)
+head(kakao_message)
+head(kakao_date)
 
+head(cbind( "User" = kakao_user_name, "message" = kakao_message))
+head(data.frame("User" = kakao_user_name, "message" = kakao_message))
+head(cbind("name" = kakao_user_name, "message" = kakao_message, "Index" = which(text_colon_index)))
+
+head(data.frame("User name" = kakao_user_name, "Message" = kakao_message, "Index" = which(text_colon_index)))
+
+b <- c("오징어", "꼴뚜기", "다랑어")
+a <- c(1,2,3,4,5)
+which(b == "꼴뚜기")
 
 # User_name_index 데이터프레임 만들기
 kakao_colon_df <- data.frame("Username"= kakao_user_name, "Message" = kakao_message, "Index" = which(text_colon_index))
@@ -103,6 +121,8 @@ kakao_df <- kakao_df %>% na.omit()
 
 
 # User name 바꿔주기
-levels(kakao_df$Username)[1:3] <- paste0("User",c(1:length(levels(kakao_df$Username)[1:3])))
+levels(kakao_df$Username)[1:2] <- paste0("User",c(1:length(levels(kakao_df$Username)[1:2])))
 
 kakao_df <- kakao_df[,-4]
+
+
